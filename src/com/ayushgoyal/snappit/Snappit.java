8d97b2b40f3bc20d74203757e043eb1ea20cbcd2
ayushgoyal09 <ayushgoyal09@gmail.com>
2014-05-15 14:37:22 -0700

    Inflate gridview using images online

--- src/com/ayushgoyal/snappit/Snappit.java
+++ src/com/ayushgoyal/snappit/Snappit.java
@@ -13,6 +13,12 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -37,11 +43,18 @@ public class Snappit extends Activity implements OnClickListener {
 
 	private ProgressDialog pDialog;
 	private static final String URL = "http://www.ayushgoyal09.com/webservice/upload_image.php";
-	private static String [] testImageURL = {
-			"http://www.ayushgoyal09.com/webservice/uploadss/IMG_20140513_224902.jpg",
-			"http://www.ayushgoyal09.com/webservice/uploadss/IMG_20140514_200659.jpg" };
-	
-	
+	private static final String URL_get_imagesList = "http://www.ayushgoyal09.com/webservice/get_all_images.php";
+	private static final String UPLOADS_FOLDER = "http://www.ayushgoyal09.com/webservice/uploadss/";
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_IMAGES = "images";
+	JSONArray images;
+
+	// private static String [] testImageURL = {
+	// "http://www.ayushgoyal09.com/webservice/uploadss/IMG_20140513_224902.jpg",
+	// "http://www.ayushgoyal09.com/webservice/uploadss/IMG_20140514_200659.jpg"
+	// };
+	private static ArrayList<String> image_urls = new ArrayList<String>();
+
 	private static final int MEDIA_TYPE_IMAGE = 1;
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	private static String mCurrentPhotoPath; // File path to the last image
@@ -57,13 +70,12 @@ public class Snappit extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_screen);
-		Log.i("size of images",""+ testImageURL.length);
+		Log.i("size of images", "" + image_urls.size());
 		thumbnails = (GridView) findViewById(R.id.image_grid);
 		takePictureButton = (Button) findViewById(R.id.camera_button);
 		takePictureButton.setOnClickListener(this);
 		new DisplayImages().execute();
-		
-		
+
 	}
 
 	@Override
@@ -294,6 +306,7 @@ public class Snappit extends Activity implements OnClickListener {
 			Toast toast = Toast.makeText(getApplicationContext(),
 					"Image added Successfully!", Toast.LENGTH_SHORT);
 			toast.show();
+			new ThumbnailAdapter(getApplicationContext()).notifyDataSetChanged();
 
 		}
 
@@ -305,25 +318,48 @@ public class Snappit extends Activity implements OnClickListener {
 		protected String doInBackground(String... params) {
 			URL url;
 			try {
-				int i=0;
-				while(i<testImageURL.length){
-					
-				
-				url = new URL(testImageURL[i]);
-				HttpURLConnection connection = (HttpURLConnection) url
-						.openConnection();
-				connection.setDoInput(true);
-				connection.connect();
-				InputStream input = connection.getInputStream();
-				//testImage = BitmapFactory.decodeStream(input);
-				allImages.add(BitmapFactory.decodeStream(input));
-				i++;
+				List<NameValuePair> args = new ArrayList<NameValuePair>();
+				JSONObject json = new JSONParser().makeHttpRequest(
+						URL_get_imagesList, "GET", args);
+				Log.i("Output", json.toString());
+
+				int success = 1;
+				if (success == 1) {
+					images = json.getJSONArray(TAG_IMAGES);
+					for (int i = 0; i < images.length(); i++) {
+						JSONObject device = images.getJSONObject(i);
+						String name = UPLOADS_FOLDER+device.getString("name");
+						Log.i("image",name);
+						image_urls.add(name);
+
+						Log.i("Imagelog: ", image_urls.get(i).toString());
+
+					}
+
+				}
+
+				int i = 0;
+				while (i < image_urls.size()) {
+
+					url = new URL(image_urls.get(i));
+					HttpURLConnection connection = (HttpURLConnection) url
+							.openConnection();
+					connection.setDoInput(true);
+					connection.connect();
+					InputStream input = connection.getInputStream();
+					testImage = BitmapFactory.decodeStream(input);
+					allImages.add(Bitmap.createScaledBitmap(testImage, 250,
+							250, true));
+					i++;
 				}
 
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -337,6 +373,7 @@ public class Snappit extends Activity implements OnClickListener {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
 			thumbnails.setAdapter(new ThumbnailAdapter(Snappit.this));
+			
 
 		}
 	}
