a614df08d79839c4c1e623ba50a35ddfc3a95481
Ayush Goyal <agoyal@tacpoint.com>
2015-03-03 17:37:54 -0800

    change access modifier

--- src/com/ayushgoyal/snappit/util/Constants.java
+++ src/com/ayushgoyal/snappit/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String URL = "http://www.ayushgoyal09.com/webservice/upload_image.php";
 	public static final String URL_get_imagesList = "http://www.ayushgoyal09.com/webservice/get_all_images.php";
 	public static final String UPLOADS_FOLDER = "http://www.ayushgoyal09.com/webservice/uploadss/";
-	private static final String TAG_SUCCESS = "success";
+	public static final String TAG_SUCCESS = "success";
 	public static final String TAG_IMAGES = "images";
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
