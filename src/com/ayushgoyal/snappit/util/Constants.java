bb85d69aa09fe546203fdfc8f167d82364adb4ab
Ayush Goyal <agoyal@tacpoint.com>
2015-03-03 18:49:10 -0800

    Create a global constant array list for albums.

--- src/com/ayushgoyal/snappit/util/Constants.java
+++ src/com/ayushgoyal/snappit/util/Constants.java
@@ -1,6 +1,11 @@
 package com.ayushgoyal.snappit.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ProgressDialog;
+
+import com.ayushgoyal.snappit.beans.AlbumBean;
 
 public class Constants {
 
@@ -13,6 +18,7 @@ public class Constants {
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	public static final String GET_ALBUMS_URL = "http://www.ayushgoyal09.com/webservice/get_all_albums.php";
+	public static List<AlbumBean> ALBUM_LIST = new ArrayList<AlbumBean>();	
 
 
 }
