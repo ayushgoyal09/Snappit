0a6d33156f80d36372d4f84ec09153a256f41d8a
Ayush Goyal <agoyal@tacpoint.com>
2015-03-03 16:19:03 -0800

    Constants data moved to class

---
+++ src/com/ayushgoyal/snappit/util/Constants.java
@@ -0,0 +1,18 @@
+package com.ayushgoyal.snappit.util;
+
+import android.app.ProgressDialog;
+
+public class Constants {
+
+	public static ProgressDialog pDialog = null;
+	public static final String URL = "http://www.ayushgoyal09.com/webservice/upload_image.php";
+	public static final String URL_get_imagesList = "http://www.ayushgoyal09.com/webservice/get_all_images.php";
+	public static final String UPLOADS_FOLDER = "http://www.ayushgoyal09.com/webservice/uploadss/";
+	private static final String TAG_SUCCESS = "success";
+	public static final String TAG_IMAGES = "images";
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	public static final String GET_ALBUMS_URL = "http://www.ayushgoyal09.com/webservice/get_all_albums.php";
+
+
+}
